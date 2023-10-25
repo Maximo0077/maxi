6becf7d68abe8228d8f911e2a925e65d9c0b5f5e
MaximoMorales <moralesmaximo2002@gmail.com>
2023-10-25 13:28:55 -0300

    item

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { NavBar } from './components/Navbar/Navabar';
+import {Navbar} from './components/Navbar/Navabar'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ItemListContainer } from './components/itemListContainer';
 import './App.jsx'
@@ -6,30 +6,25 @@ import ItemDetailContainer from './components/itemDetailContainer';
 import { CartProvider } from './context/CartContext';
 import { Cart } from './components/cart';
 
-
-
-
-
-
 function App () {
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <CartProvider>
-          <NavBar />
-          <Routes>
-            <Route path='/' element ={<ItemListContainer/>} />
-            <Route path='/category/:categoryId' element ={<ItemListContainer/>} />
-            <Route path='/item/:id' element ={<ItemDetailContainer />} />
-            <Route path='/cart' element={< Cart />} />
-            <Route path='*' element ={ <h1>404 NOT FOUND</h1> } />
-          </Routes>
-        </CartProvider>
-      </BrowserRouter>
-      
-      
-    </div>
-  );
+return (
+<div className="App">
+<BrowserRouter>
+<CartProvider>
+<Navbar />
+<Routes>
+<Route path='/' element ={<ItemListContainer/>} />
+<Route path='/category/:categoryId' element ={<ItemListContainer/>} />
+<Route path='/item/:id' element ={<ItemDetailContainer />} />
+<Route path='/cart' element={< Cart />} />
+<Route path='*' element ={ <h1>404 NOT FOUND</h1> } />
+</Routes>
+</CartProvider>
+</BrowserRouter>
+
+
+</div>
+);
 }
 export default App
--- src/components/itemListContainer/index.jsx
+++ src/components/itemListContainer/index.jsx
@@ -2,43 +2,43 @@ import { useState, useEffect } from "react";
 import ItemList from "../itemList";
 import { useParams } from "react-router-dom";
 import { getDocs, collection, query, where } from "firebase/firestore";
-import { db } from "../firebase/config.js";
+import { db } from "../../firebase/config"
 
 export const ItemListContainer = ({greeting} ) => {
-    const [products, setProducts] = useState ([])
-    const [loading, setLoading] = useState(true)
+const [products, setProducts] = useState ([])
+const [loading, setLoading] = useState(true)
 
-    const { categoryId } = useParams()
+const { categoryId } = useParams()
 
-    useEffect(() => {
-        setLoading(true)
+useEffect(() => {
+setLoading(true)
 
-        const productosRef = colection (db, "productos")
-            ? query(collection(db, 'products'), where('category', '==', categoryId) )
-            : collection(db, 'products')
-        
-        getDocs(productosRef)
-            .then(response => {
-                const productsAdapted = response.docs.map(doc => {
-                    const data = doc.data()
-                    return { id: doc.id, ...data }
-                })
-                setProducts(productsAdapted)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [ categoryId ])
+const productosRef = categoryId
+? query(collection(db, 'products'), where('category', '==', categoryId) )
+: collection(db, 'products')
 
-    return(
-        <div>
-            <h1>
-                {greeting}
-            </h1>
-            <ItemList products={products}/>
-        </div>
-    )
+getDocs(productosRef)
+.then(response => {
+const productsAdapted = response.docs.map(doc => {
+const data = doc.data()
+return { id: doc.id, ...data }
+})
+setProducts(productsAdapted)
+})
+.catch(error => {
+console.log(error)
+})
+.finally(() => {
+setLoading(false)
+})
+}, [ categoryId ])
+
+return(
+<div>
+<h1>
+{greeting}
+</h1>
+<ItemList products={products}/>
+</div>
+)
 }
