cae33e4b8c985a6bcd6b2ac4d11361e0ba2036da
MaximoMorales <moralesmaximo2002@gmail.com>
2023-10-25 14:23:07 -0300

    entrega3

--- src/components/itemListContainer/index.jsx
+++ src/components/itemListContainer/index.jsx
@@ -5,40 +5,40 @@ import { getDocs, collection, query, where } from "firebase/firestore";
 import { db } from "../../firebase/config"
 
 export const ItemListContainer = ({greeting} ) => {
-const [products, setProducts] = useState ([])
-const [loading, setLoading] = useState(true)
+    const [products, setProducts] = useState ([])
+    const [loading, setLoading] = useState(true)
 
-const { categoryId } = useParams()
+    const { categoryId } = useParams()
 
-useEffect(() => {
-setLoading(true)
+    useEffect(() => {
+        setLoading(true)
 
-const productosRef = categoryId
-? query(collection(db, 'products'), where('category', '==', categoryId) )
-: collection(db, 'products')
+        const productosRef = categoryId
+            ? query(collection(db, 'products'), where('category', '==', categoryId) )
+            : collection(db, 'products')
+        
+        getDocs(productosRef)
+            .then(response => {
+                const productsAdapted = response.docs.map(doc => {
+                    const data = doc.data()
+                    return { id: doc.id, ...data }
+                })
+                setProducts(productsAdapted)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }, [ categoryId ])
 
-getDocs(productosRef)
-.then(response => {
-const productsAdapted = response.docs.map(doc => {
-const data = doc.data()
-return { id: doc.id, ...data }
-})
-setProducts(productsAdapted)
-})
-.catch(error => {
-console.log(error)
-})
-.finally(() => {
-setLoading(false)
-})
-}, [ categoryId ])
-
-return(
-<div>
-<h1>
-{greeting}
-</h1>
-<ItemList products={products}/>
-</div>
-)
+    return(
+        <div>
+            <h1>
+                {greeting}
+            </h1>
+            <ItemList products={products}/>
+        </div>
+    )
 }
